2e5355ba288f7dba9b99a58e7043673142f5f791
00kae <trenguyen@ucdavis.edu>
2024-03-13 13:12:43 -0700

    ter5

--- ECS 163 Final Project/Template/templates/bargraph.js
+++ ECS 163 Final Project/Template/templates/bargraph.js
@@ -6,10 +6,37 @@ let typeMargin = {top: 10, right: 30, bottom: 30, left: 60},
     typeWidth = width - typeMargin.left - typeMargin.right,
     typeHeight = height-450 - typeMargin.top - typeMargin.bottom;
 
+const svg = d3.select("svg")
+
 const g3 = svg.append("g")
     .attr("width", typeWidth + typeMargin.left + typeMargin.right)
     .attr("height", typeHeight + typeMargin.top + typeMargin.bottom)
     .attr("transform", `translate(${typeMargin.left}, ${typeTop})`)
+
+
+
+const populationTotal = 300e6;
+const heartFailureTotal = 6e6;
+
+const populationByAge = populationDistribution.map(population => population * populationTotal);
+const heartFailureByAge = heartFailureDistribution.map(heartfail => heartfail * heartFailureTotal);
+
+const barWidth = 50;
+
+const x = 50;
+const y = 350;
+
+const colors = ['blue', 'red'];
+
+    for (let i = 0; i < ageGroups.length; i++) {
+
+        const populationHeight = (populationByAge[i] / populationTotal) * 300;
+            svg.innerHTML += `<rect x="${x}" y="${y - populationHeight}" width="${barWidth}" height="${populationHeight}" fill="${'blue'}" />`;
+
+        const heartFailureHeight = (heartFailureByAge[i] / populationTotal) * 300;
+            svg.innerHTML += `<rect x="${x}" y="${y - populationHeight - heartFailureHeight}" width="${barWidth}" height="${heartFailureHeight}" fill="${'red'}" />`;
+
+    }
 
 // Chart title
 g3.append("text")
@@ -17,7 +44,7 @@ g3.append("text")
 .attr("y", -10)
 .attr("font-size", "20px")
 .attr("text-anchor", "middle")
-.text("Total Battle Statistics of Each Pokemon Type")
+.text("Total US population to Total with heart failure")
 
 // X label
 g3.append("text")
@@ -25,4 +52,5 @@ g3.append("text")
 .attr("y", typeHeight + 50)
 .attr("font-size", "15px")
 .attr("text-anchor", "middle")
-.text("Type")
+.text("Number of Individuals")
+
